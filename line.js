3df39b6e1f002cfbf659234eaf31ecb7e4ee76f6
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2019-12-22 09:18:31 +0800

    Update line.js

--- line.js
+++ line.js
@@ -1,13 +1,13 @@
 'use strict'
 var line = require("@line/bot-sdk")
-var app = require("express")
+var express = require("express")
 const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.CHANNEL_SECRET,
   };
 
   const client = new line.Client(config);
-
+  const app = express();
   
   // register a webhook handler with middleware
   // about the middleware, please refer to doc
