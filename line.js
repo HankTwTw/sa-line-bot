d6b48cb65e48b2189b8a5ace3da8af739d8bd1be
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2019-12-22 09:32:41 +0800

    Update line.js

--- line.js
+++ line.js
@@ -12,6 +12,7 @@ const config = {
   // register a webhook handler with middleware
   // about the middleware, please refer to doc
   app.post('/', line.middleware(config), (req, res) => {
+        console.log(config)
         console.log(req.body.events)
         Promise.all(req.body.events.map(handleEvent))
         .then((result) => res.json(result))
