d35a0dae9387ffc5720fe5ea5e368ccd6d446c9f
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2019-12-22 09:41:28 +0800

    Update line.js

--- line.js
+++ line.js
@@ -11,7 +11,7 @@ const config = {
   
   // register a webhook handler with middleware
   // about the middleware, please refer to doc
-  app.post('/callback', line.middleware(config), (req, res) => {
+  app.post('/', line.middleware(config), (req, res) => {
         console.log(config)
         console.log(process.env.PORT)
         console.log(req.body.events)
@@ -28,7 +28,7 @@ const config = {
     if (event.type !== 'message' || event.message.type !== 'text') {
       return Promise.resolve(null);
     }
-    const echo = { type: 'text', text: "event.message.text" };
+    const echo = { type: 'text', text: event.message.text };
 
     return client.replyMessage(event.replyToken, echo);
   }
