a08645a2ac0813f07626dbdf5aeab2c6b5388a38
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2019-12-22 09:04:03 +0800

    Initial commit

---
+++ line.js
@@ -0,0 +1,39 @@
+'use strict'
+var line = require("@line/bot-sdk")
+var express = require("express")
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+  };
+
+  const client = new line.Client(config);
+
+  
+  // register a webhook handler with middleware
+  // about the middleware, please refer to doc
+  app.post('/callback', line.middleware(config), (req, res) => {
+        console.log(req.body.events)
+        Promise.all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+  });
+  
+
+  function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    const echo = { type: 'text', text: event.message.text };
+
+    return client.replyMessage(event.replyToken, echo);
+  }
+  
+
+  const port = process.env.PORT || 8080;
+  express.listen(port, () => {
+    console.log(`listening on ${port}`);
+  });
