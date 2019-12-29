268df0aba793005f37efc0044f222f7670c3107b
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2019-12-29 12:08:29 +0800

    Update liff-scan-qrcode.js

--- public/liff-scan-qrcode.js
+++ public/liff-scan-qrcode.js
@@ -107,7 +107,8 @@ function scan(){
     });
 }
 function send_money(){
-    var dollar = document.getElementById("typing_dollar").value
+    console.log(document.getElementById("typing_dollar").value)
+    var dollar = parseInt(document.getElementById("typing_dollar").value)
     if(dollar>have_money){window.alert("您的錢只有"+have_money+"哦，請先參加活動賺取樂幣")}
     else if(confirm("確定要給?"+name+dollar+"元嗎? 你有"+have_money+"元")){
        firebase.database().ref("user/"+scan_user_Id).once("value",function(snap){
